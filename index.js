09cc1f165d5ac3c527d49d946dbe8771e37d83fa
ArturIsunc <121034294+ArturIsunc@users.noreply.github.com>
2022-12-26 01:25:43 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+1.
+//let x=prompt("greq tiv")
+//let y=0
+//if(x>0) {alert(x)}
+//else {alert(Math.abs(x))}
+
+2.
+//let a=1
+//let b=2
+//let c=3
+//let max=Math.max(a,b,c)
+//let min=Math.min(a,b,c)
+//alert(""+ max +"," + min)
+
+
+3.
+let login="user123"
+let parol="123"
+let l=prompt("MUTQAGREQ DZER LOGIN@")
+let p=prompt("MUTQAGREQ DZER PASSWORD@")
+if (l.toLowerCase()===login&&p===parol){alert("DUQ BAREHAJOX MUTQ EQ GORCEL")}
+
+//4.
+
+ //let x=prompt("qani tarekan eq")
+ //if(x<18){alert("DUQ SHAT POQR EQ")}
+    //else if(x>18&&x<63){alert("DUQ BAREHAJOX GRANCVEL EQ")}
+    //else if(x>63){alert("DUQ SHAT MEC EQ")}
+        //else{alert("ERROR")}
